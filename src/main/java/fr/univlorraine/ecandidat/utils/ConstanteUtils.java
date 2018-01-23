755b8831c132cbcf48a2eabf7b377fff70a48efa
Kevin Hergalant <kevin.hergalant@univ-lorraine.fr>
2018-01-23 15:01:55 +0100

    modification du WS checkine

--- src/main/java/fr/univlorraine/ecandidat/utils/ConstanteUtils.java
+++ src/main/java/fr/univlorraine/ecandidat/utils/ConstanteUtils.java
@@ -1,19 +1,13 @@
-/**
- *  ESUP-Portail eCandidat - Copyright (c) 2016 ESUP-Portail consortium
- *
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+/** ESUP-Portail eCandidat - Copyright (c) 2016 ESUP-Portail consortium
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
 package fr.univlorraine.ecandidat.utils;
 
 /** Classe de constantes
@@ -343,9 +337,9 @@ public class ConstanteUtils {
 
 	/* WS INES */
 	public static String WS_INES_CHECK_SERVICE = "checkInes.urlService";
-	public static String WS_INES_URI = "/checkINE/type/{type}/ine/{ine}/key/{key}";
+	public static String WS_INES_URI = "/checkINE/type/{type}/ine/{ine}/cle/{cle}";
 	public static String WS_INES_PARAM_INE = "ine";
-	public static String WS_INES_PARAM_KEY = "key";
+	public static String WS_INES_PARAM_KEY = "cle";
 	public static String WS_INES_PARAM_TYPE = "type";
 	public static String WS_INES_PARAM_TYPE_INES = "INES";
 
