8b7687e7ff181d56c33af3aea03251411301056b
Kevin Hergalant <kevin.hergalant@univ-lorraine.fr>
2018-07-05 16:19:31 +0200

    nettoyage batch au démarrage

--- src/main/java/fr/univlorraine/ecandidat/controllers/BatchController.java
+++ src/main/java/fr/univlorraine/ecandidat/controllers/BatchController.java
@@ -1,19 +1,13 @@
-/**
- *  ESUP-Portail eCandidat - Copyright (c) 2016 ESUP-Portail consortium
- *
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+/** ESUP-Portail eCandidat - Copyright (c) 2016 ESUP-Portail consortium
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
 package fr.univlorraine.ecandidat.controllers;
 
 import java.time.DayOfWeek;
@@ -332,7 +326,7 @@ public class BatchController {
 			if (batch.getCodBatch().equals(NomenclatureUtils.BATCH_SI_SCOL)) {
 				siScolController.syncSiScol(batchHisto);
 			} else if (batch.getCodBatch().equals(NomenclatureUtils.BATCH_NETTOYAGE)) {
-				nettoyageBatch();
+				nettoyageBatch(4);
 			} else if (batch.getCodBatch().equals(NomenclatureUtils.BATCH_APP_EN_MAINT)) {
 				parametreController.changeMaintenanceParam(true);
 			} else if (batch.getCodBatch().equals(NomenclatureUtils.BATCH_APP_EN_SERVICE)) {
@@ -376,16 +370,27 @@ public class BatchController {
 	}
 
 	/** Batch de nettoyage des batchs */
-	private void nettoyageBatch() {
+	public void nettoyageBatch(final Integer plusHour) {
 		List<BatchHisto> listHisto = batchHistoRepository.findByStateBatchHisto(ConstanteUtils.BATCH_RUNNING);
 		listHisto.forEach(batchHisto -> {
-			LocalDateTime now = LocalDateTime.now();
-			LocalDateTime histPlusHour = batchHisto.getDateDebBatchHisto().plusHours(new Long(4));
-			if (histPlusHour.isBefore(now)) {
-				batchHisto.setStateBatchHisto(ConstanteUtils.BATCH_INTERRUPT);
-				batchHisto.setDateFinBatchHisto(now);
-				batchHistoRepository.saveAndFlush(batchHisto);
+			if (plusHour.equals(0)) {
+				interruptBatch(batchHisto);
+			} else {
+				LocalDateTime histPlusHour = batchHisto.getDateDebBatchHisto().plusHours(new Long(plusHour));
+				if (histPlusHour.isBefore(LocalDateTime.now())) {
+					interruptBatch(batchHisto);
+				}
 			}
 		});
+	}
+
+	/** Interrompt un batch
+	 * 
+	 * @param batchHisto
+	 */
+	private void interruptBatch(final BatchHisto batchHisto) {
+		batchHisto.setStateBatchHisto(ConstanteUtils.BATCH_INTERRUPT);
+		batchHisto.setDateFinBatchHisto(LocalDateTime.now());
+		batchHistoRepository.saveAndFlush(batchHisto);
 	}
 }
--- src/main/resources/db/migration/V2_2_10_2__Dates.sql
+++ src/main/resources/db/migration/V2_2_10_2__Dates.sql
@@ -8,13 +8,13 @@ ALTER TABLE candidature
 	ADD COLUMN dat_new_retour_cand DATE NULL DEFAULT NULL COMMENT 'nouvelle date de retour du dossier' AFTER dat_incomplet_dossier_cand;
 
 --
--- Modification table candidature
+-- Modification table formation
 --
 ALTER TABLE formation
 	ADD COLUMN dat_confirm_list_comp_form DATE NULL DEFAULT NULL COMMENT 'date limite de confirmation lors de la gestion automatique des listes complémentaires' AFTER dat_confirm_form;
 
 	--
--- Modification table candidature
+-- Modification table centre_candidature
 --	
 ALTER TABLE centre_candidature
 	ADD COLUMN dat_confirm_list_comp_ctr_cand DATE NULL DEFAULT NULL COMMENT 'date limite de confirmation lors de la gestion automatique des listes complémentaires' AFTER dat_confirm_ctr_cand;
