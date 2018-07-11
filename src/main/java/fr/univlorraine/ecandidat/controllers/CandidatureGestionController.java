ed59ab5d2f26a0861bdd2fc2f8223d30c0634f8d
Kevin Hergalant <kevin.hergalant@univ-lorraine.fr>
2018-07-11 17:08:56 +0200

    update license

--- src/main/java/fr/univlorraine/ecandidat/controllers/CandidatureGestionController.java
+++ src/main/java/fr/univlorraine/ecandidat/controllers/CandidatureGestionController.java
@@ -1,13 +1,19 @@
-/** ESUP-Portail eCandidat - Copyright (c) 2016 ESUP-Portail consortium
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. */
+/**
+ *  ESUP-Portail eCandidat - Copyright (c) 2016 ESUP-Portail consortium
+ *
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package fr.univlorraine.ecandidat.controllers;
 
 import java.io.InputStream;
@@ -24,7 +30,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import fr.univlorraine.ecandidat.entities.ecandidat.BatchHisto;
@@ -265,8 +270,7 @@ public class CandidatureGestionController {
 		if (nbOpi == null || nbOpi.equals(0)) {
 			nbOpi = Integer.MAX_VALUE;
 		}
-		Pageable pageable = new PageRequest(0, nbOpi);
-		List<Candidat> listeCandidat = candidatRepository.findOpi(campagne.getIdCamp(), pageable);
+		List<Candidat> listeCandidat = candidatRepository.findOpi(campagne.getIdCamp(), new PageRequest(0, nbOpi));
 		batchController.addDescription(batchHisto, "Lancement batch, deversement de " + listeCandidat.size() + " OPI");
 		Integer i = 0;
 		Integer cpt = 0;
