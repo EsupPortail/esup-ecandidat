ff8a066154a2a7933ebde4266d593246e0c77dc7
Kevin Hergalant <kevin.hergalant@univ-lorraine.fr>
2019-03-19 09:46:44 +0100

    classement des motivations d'avis par code

--- src/main/java/fr/univlorraine/ecandidat/vaadin/form/combo/ComboBoxMotivationAvis.java
+++ src/main/java/fr/univlorraine/ecandidat/vaadin/form/combo/ComboBoxMotivationAvis.java
@@ -1,18 +1,14 @@
 /**
- *  ESUP-Portail eCandidat - Copyright (c) 2016 ESUP-Portail consortium
- *
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * ESUP-Portail eCandidat - Copyright (c) 2016 ESUP-Portail consortium
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package fr.univlorraine.ecandidat.vaadin.form.combo;
 
@@ -21,11 +17,11 @@ import java.util.List;
 import com.vaadin.data.util.BeanItemContainer;
 
 import fr.univlorraine.ecandidat.entities.ecandidat.MotivationAvis;
+import fr.univlorraine.ecandidat.entities.ecandidat.MotivationAvis_;
 import fr.univlorraine.ecandidat.vaadin.form.RequiredComboBox;
 
 /**
  * ComboBox pour les MotivationAvis
- *
  * @author Kevin Hergalant
  */
 @SuppressWarnings("serial")
@@ -50,28 +46,28 @@ public class ComboBoxMotivationAvis extends RequiredComboBox<MotivationAvis> {
 	public void setMotivationAvis(final List<MotivationAvis> listeMotivation) {
 		container.removeAllItems();
 		container.addAll(listeMotivation);
+		container.sort(new Object[] { MotivationAvis_.codMotiv.getName() }, new boolean[] { true });
 	}
 
 	/**
 	 * SI la box n'est pas utilisé ou utilisé
-	 *
 	 * @param need
 	 * @param motiv
 	 */
 	public void setBoxNeeded(final Boolean need, final MotivationAvis motiv) {
 		if (need) {
-			this.setVisible(true);
-			this.setRequired(true);
-			this.setRequiredError(error);
-			this.setNullSelectionAllowed(false);
+			setVisible(true);
+			setRequired(true);
+			setRequiredError(error);
+			setNullSelectionAllowed(false);
 			if (motiv != null) {
 				setValue(motiv);
 			}
 		} else {
-			this.setVisible(false);
-			this.setRequired(false);
-			this.setRequiredError(null);
-			this.setNullSelectionAllowed(true);
+			setVisible(false);
+			setRequired(false);
+			setRequiredError(null);
+			setNullSelectionAllowed(true);
 			this.setValue(null);
 		}
 	}
