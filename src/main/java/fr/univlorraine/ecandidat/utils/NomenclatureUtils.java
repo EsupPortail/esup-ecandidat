7931ed927a7e816918798f1e92e6f8004e1c53a2
Kevin Hergalant <kevin.hergalant@univ-lorraine.fr>
2019-03-26 10:52:24 +0100

    correction nomenclature

--- src/main/java/fr/univlorraine/ecandidat/utils/NomenclatureUtils.java
+++ src/main/java/fr/univlorraine/ecandidat/utils/NomenclatureUtils.java
@@ -27,7 +27,7 @@ public class NomenclatureUtils {
 
 	/* Version */
 	public static final String VERSION_NOMENCLATURE_COD = "VERSION_NOMENCLATURE";
-	public static final String VERSION_NOMENCLATURE_VAL = "2.3.0.1";
+	public static final String VERSION_NOMENCLATURE_VAL = "2.3.0.2";
 	public static final String VERSION_NO_VERSION_VAL = "-";
 	public static final String VERSION_APPLICATION_COD = "VERSION_APPLICATION";
 	public static final String VERSION_DB = "VERSION_DB";
@@ -137,10 +137,8 @@ public class NomenclatureUtils {
 
 	/* Mail */
 	public static final String MAIL_GEN_VAR = "libelleCampagne";
-	public static final String MAIL_CANDIDAT_GEN_VAR =
-		"candidat.civilite;candidat.numDossierOpi;candidat.nomPat;candidat.nomUsu;candidat.prenom;candidat.autrePrenom;candidat.ine;candidat.cleIne;candidat.datNaiss;candidat.libVilleNaiss;candidat.libLangue;candidat.tel;candidat.telPort";
-	public static final String MAIL_FORMATION_GEN_VAR =
-		"formation.code;formation.libelle;formation.codEtpVetApo;formation.codVrsVetApo;formation.libApo;formation.motCle;formation.datDebDepot;formation.datFinDepot;formation.datPreAnalyse;formation.datRetour;formation.datJury;formation.datPubli;formation.datConfirm";
+	public static final String MAIL_CANDIDAT_GEN_VAR = "candidat.civilite;candidat.numDossierOpi;candidat.nomPat;candidat.nomUsu;candidat.prenom;candidat.autrePrenom;candidat.ine;candidat.cleIne;candidat.datNaiss;candidat.libVilleNaiss;candidat.libLangue;candidat.tel;candidat.telPort";
+	public static final String MAIL_FORMATION_GEN_VAR = "formation.code;formation.libelle;formation.codEtpVetApo;formation.codVrsVetApo;formation.libApo;formation.motCle;formation.datDebDepot;formation.datFinDepot;formation.datPreAnalyse;formation.datRetour;formation.datJury;formation.datPubli;formation.datConfirm";
 	public static final String MAIL_COMMISSION_GEN_VAR = "commission.libelle;commission.mail;commission.adresse;commission.tel;commission.fax;commission.commentaireRetour;commission.signataire";
 	public static final String MAIL_DOSSIER_GEN_VAR = "dossier.dateReception";
 
