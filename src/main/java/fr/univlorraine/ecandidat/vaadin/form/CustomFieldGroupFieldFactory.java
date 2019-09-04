53c150e004e2994160c00e0a9aec74b3e7ea5224
Kevin Hergalant <kevin.hergalant@univ-lorraine.fr>
2019-09-04 12:50:33 +0200

    filtre sur les cat exo

--- src/main/java/fr/univlorraine/ecandidat/vaadin/form/CustomFieldGroupFieldFactory.java
+++ src/main/java/fr/univlorraine/ecandidat/vaadin/form/CustomFieldGroupFieldFactory.java
@@ -1,18 +1,14 @@
 /**
- *  ESUP-Portail eCandidat - Copyright (c) 2016 ESUP-Portail consortium
- *
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * ESUP-Portail eCandidat - Copyright (c) 2016 ESUP-Portail consortium
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package fr.univlorraine.ecandidat.vaadin.form;
 
@@ -21,6 +17,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 import javax.annotation.Resource;
 
@@ -185,7 +182,7 @@ public class CustomFieldGroupFieldFactory extends DefaultFieldGroupFieldFactory 
 		}
 		/* La valeur est une catégorie exonération/extracommunautaire */
 		else if (dataType == SiScolCatExoExt.class) {
-			return fieldType.cast(new RequiredComboBox<>(cacheController.getListeCatExoExt(), SiScolCatExoExt.class));
+			return fieldType.cast(new RequiredComboBox<>(cacheController.getListeCatExoExt().stream().filter(e -> e.getTemEnSveCatExoExt()).collect(Collectors.toList()), SiScolCatExoExt.class));
 		}
 		/* La valeur est un type de traitement */
 		else if (dataType == TypeTraitement.class) {
